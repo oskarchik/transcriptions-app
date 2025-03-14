ebaa6e9bc81c669e21614ddcca101ac391f231db
Oscar Sanz <oscarsanz@Oscars-MacBook-Pro.local>
2025-03-14 16:36:45 +0100

    feat: create auth page with conditional rendering for login and registration forms

---
+++ src/pages/auth.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="w-full max-w-md mx-auto p-6 bg-white rounded-md shadow-lg">
+      <div class="flex flex-col justify-center">
+        <!-- Condicionalmente renderizar el formulario de registro o inicio de sesión -->
+        <RegisterForm v-if="isRegistering" />
+        <LoginForm v-else />
+
+        <!-- Enlace para alternar entre el formulario de registro e inicio de sesión -->
+        <div class="text-center mt-4">
+          <p class="text-sm text-gray-500">
+            <span v-if="isRegistering">Already have an account?</span>
+            <span v-else>Don't have an account?</span>
+            <a
+              href="#"
+              @click.prevent="toggleForm"
+              class="text-indigo-600 hover:text-indigo-700"
+            >
+              {{ isRegistering ? "Login here" : "Register here" }}
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import RegisterForm from "@/components/custom/registerForm.vue";
+import LoginForm from "@/components/custom/loginForm.vue";
+
+const isRegistering = ref(false);
+
+const toggleForm = () => {
+  isRegistering.value = !isRegistering.value;
+};
+</script>
