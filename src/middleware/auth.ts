21a02bf622fb69075cec72ced57a623a06ac6e04
Oscar Sanz <oscarsanz@Oscars-MacBook-Pro.local>
2025-03-14 18:46:04 +0100

    feat: add authentication middleware to protect routes and redirect unauthenticated users

---
+++ src/middleware/auth.ts
@@ -0,0 +1,10 @@
+// middleware/auth.ts
+import { defineNuxtRouteMiddleware } from "#app";
+import { useAuth } from "@/stores/auth";
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const authStore = useAuth();
+
+  if (!authStore.isAuthenticated) {
+    return navigateTo("/auth");
+  }
+});
